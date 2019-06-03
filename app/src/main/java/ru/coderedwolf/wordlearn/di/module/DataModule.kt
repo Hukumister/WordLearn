2408d68cc83a2f30be467c0affca1a77b08187bf
nmZaltsman <zaltsman.nikita@gmail.com>
2019-06-03 23:48:59 +0700

    + add data module

---
+++ app/src/main/java/ru/coderedwolf/wordlearn/di/module/DataModule.kt
@@ -0,0 +1,20 @@
+package ru.coderedwolf.wordlearn.di.module
+
+import ru.coderedwolf.wordlearn.domain.data.reporitory.WordsCategoryRepository
+import ru.coderedwolf.wordlearn.domain.data.reporitory.WordsCategoryRepositoryImpl
+import ru.coderedwolf.wordlearn.domain.interactors.WordsCategoryInteractor
+import ru.coderedwolf.wordlearn.domain.interactors.WordsCategoryInteractorImpl
+import ru.coderedwolf.wordlearn.domain.mappers.CategoryMapper
+import toothpick.config.Module
+
+/**
+ * @author CodeRedWolf. Date 08.05.2019.
+ */
+class DataModule : Module() {
+    init {
+
+        bind(WordsCategoryRepository::class.java).to(WordsCategoryRepositoryImpl::class.java)
+        bind(WordsCategoryInteractor::class.java).to(WordsCategoryInteractorImpl::class.java)
+        bind(CategoryMapper::class.java).singletonInScope()
+    }
+}
