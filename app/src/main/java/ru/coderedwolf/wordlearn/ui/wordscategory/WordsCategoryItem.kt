c0c09bac81a894e192c33be8df66622ba496d0bb
nmZaltsman <zaltsman.nikita@gmail.com>
2019-06-04 22:28:50 +0700

    * fix bug with overdraw category item

--- app/src/main/java/ru/coderedwolf/wordlearn/ui/wordscategory/WordsCategoryItem.kt
+++ app/src/main/java/ru/coderedwolf/wordlearn/ui/wordscategory/WordsCategoryItem.kt
@@ -1,23 +1,37 @@
 package ru.coderedwolf.wordlearn.ui.wordscategory
 
-import com.bumptech.glide.Glide
 import com.xwray.groupie.kotlinandroidextensions.Item
 import com.xwray.groupie.kotlinandroidextensions.ViewHolder
-import kotlinx.android.synthetic.main.item_category.view.*
+import kotlinx.android.synthetic.main.item_category.*
+import org.jetbrains.anko.onCheckedChange
 import ru.coderedwolf.wordlearn.R
 import ru.coderedwolf.wordlearn.model.Category
 
 /**
  * @author CodeRedWolf. Date 02.05.2019.
  */
+
 class WordsCategoryItem(val category: Category) : Item() {
 
+    var onChecked: (WordsCategoryItem, Boolean) -> Unit = { _, _ -> }
+
     override fun bind(viewHolder: ViewHolder, position: Int) {
-        viewHolder.itemView.categoryName.text = category.name
-        Glide.with(viewHolder.itemView)
-                .load(category.image)
-                .into(viewHolder.itemView.categoryImage)
+        viewHolder.checkbox.onCheckedChange { _, isChecked -> onChecked(this@WordsCategoryItem, isChecked) }
+        viewHolder.checkbox.isChecked = category.isStudy
+        viewHolder.categoryName.text = category.name
     }
 
     override fun getLayout() = R.layout.item_category
+
+    override fun equals(other: Any?): Boolean {
+        return if (other is WordsCategoryItem) {
+            other.category == category
+        } else {
+            false
+        }
+    }
+
+    override fun hashCode(): Int {
+        return category.hashCode()
+    }
 }
