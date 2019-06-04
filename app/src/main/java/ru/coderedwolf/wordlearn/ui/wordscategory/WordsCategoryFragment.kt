cccb362815e71c3998e6ba2f180f05db5a2d3c6c
nmZaltsman <zaltsman.nikita@gmail.com>
2019-06-04 22:27:09 +0700

    * fix words category view

--- app/src/main/java/ru/coderedwolf/wordlearn/ui/wordscategory/WordsCategoryFragment.kt
+++ app/src/main/java/ru/coderedwolf/wordlearn/ui/wordscategory/WordsCategoryFragment.kt
@@ -1,22 +1,26 @@
 package ru.coderedwolf.wordlearn.ui.wordscategory
 
 import android.os.Bundle
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.arellomobile.mvp.presenter.InjectPresenter
 import com.arellomobile.mvp.presenter.ProvidePresenter
 import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.kotlinandroidextensions.ViewHolder
 import kotlinx.android.synthetic.main.fragment_words_category_list.*
+import org.jetbrains.anko.onClick
 import ru.coderedwolf.wordlearn.R
-import ru.coderedwolf.wordlearn.extension.gone
+import ru.coderedwolf.wordlearn.extension.visibile
 import ru.coderedwolf.wordlearn.model.Category
 import ru.coderedwolf.wordlearn.presentation.WordsCategoryPresenter
 import ru.coderedwolf.wordlearn.ui.base.BaseFragment
-import toothpick.Toothpick
 
 /**
  * @author CodeRedWolf. Date 01.05.2019.
  */
-class WordsCategoryFragment : BaseFragment(), WordsCategoryView {
+class WordsCategoryFragment : BaseFragment(),
+        WordsCategoryView,
+        CreateCategoryDialog.OnCreateCategory {
 
     override val layoutRes = R.layout.fragment_words_category_list
 
@@ -29,9 +33,14 @@ class WordsCategoryFragment : BaseFragment(), WordsCategoryView {
 
     private val categoryAdapter = GroupAdapter<ViewHolder>()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        addCategoryFab.onCli
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        addCategoryFab.onClick { presenter.onClickAddCategory() }
+        categoryList.apply {
+            layoutManager = LinearLayoutManager(requireContext())
+            adapter = categoryAdapter
+            setHasFixedSize(true)
+        }
         categoryAdapter.setOnItemClickListener { item, _ ->
             if (item is WordsCategoryItem) {
                 presenter.onClickCategory(item.category)
@@ -39,13 +48,25 @@ class WordsCategoryFragment : BaseFragment(), WordsCategoryView {
         }
     }
 
-    override fun showCategory(category: Category, index: Int) = categoryAdapter.add(index, WordsCategoryItem(category))
+    override fun onCreateCategory(name: String) = presenter.onCreateCategory(name)
+
+    override fun addCategory(index: Int, category: Category) = categoryAdapter.add(index, WordsCategoryItem(category))
 
     override fun showCreateCategoryDialog() = CreateCategoryDialog.show(childFragmentManager)
 
-    override fun showCategoryList(list: List<Category>) = categoryAdapter.addAll(list.map { WordsCategoryItem(it) })
+    override fun addAllCategory(list: List<Category>) = categoryAdapter.addAll(list.map { mapToItem(it) })
 
-    override fun showLoading(show: Boolean) {
-        progressBar.gone(!show)
+    override fun updateCategoryList(list: List<Category>) = categoryAdapter.updateAsync(list.map { mapToItem(it) })
+
+    override fun showLoading(show: Boolean) = progressBar.visibile(show)
+
+    override fun onBackPressed() = presenter.onBackPressed()
+
+    private fun mapToItem(it: Category): WordsCategoryItem {
+        return WordsCategoryItem(it).apply {
+            onChecked = { item, isChecked ->
+                presenter.onCheckedCategory(item, isChecked)
+            }
+        }
     }
 }
