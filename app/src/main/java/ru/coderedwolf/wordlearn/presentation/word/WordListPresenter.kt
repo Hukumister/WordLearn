fd450245a9d284e5673b12642d0012dce27ba406
nmZaltsman <zaltsman.nikita@gmail.com>
2019-06-06 10:48:42 +0700

    * add logic for presentation of word list

--- app/src/main/java/ru/coderedwolf/wordlearn/presentation/word/WordListPresenter.kt
+++ app/src/main/java/ru/coderedwolf/wordlearn/presentation/word/WordListPresenter.kt
@@ -1,21 +1,39 @@
 package ru.coderedwolf.wordlearn.presentation.word
 
+import moxy.InjectViewState
+import ru.coderedwolf.wordlearn.Screens
 import ru.coderedwolf.wordlearn.base.BasePresenter
+import ru.coderedwolf.wordlearn.di.FlowRouter
 import ru.coderedwolf.wordlearn.di.PrimitiveWrapper
+import ru.coderedwolf.wordlearn.di.provider.qualifier.CategoryId
+import ru.coderedwolf.wordlearn.di.provider.qualifier.CategoryName
+import ru.coderedwolf.wordlearn.domain.interactors.WordInteractor
 import javax.inject.Inject
 
 /**
  * @author CodeRedWolf. Date 04.06.2019.
  */
+@InjectViewState
 class WordListPresenter @Inject constructor(
-        categoryIdWrapper: PrimitiveWrapper<Long>,
-        categoryNameWrapper: PrimitiveWrapper<String>
+        @CategoryId categoryIdWrapper: PrimitiveWrapper<Long>,
+        @CategoryName categoryNameWrapper: PrimitiveWrapper<String>,
+        private val flowRouter: FlowRouter,
+        private val wordInteractor: WordInteractor
 ) : BasePresenter<WordListView>() {
 
-    private val categorId: Long = categoryIdWrapper.value
-    private val categorName: String = categoryNameWrapper.value
+    private val categoryId: Long = categoryIdWrapper.value
+    private val categoryName: String = categoryNameWrapper.value
 
+    override fun onFirstViewAttach() = launchUI {
+        viewState.showLoading(true)
+        viewState.showTitle(categoryName)
+        val list = wordInteractor.findAllPreviewByCategoryId(categoryId)
+        viewState.showEmptyPlaceHolder(list.isEmpty())
+        viewState.showWords(list)
+        viewState.showLoading(false)
+    }
 
+    override fun onBackPressed() = flowRouter.finishFlow()
 
-
+    fun onClickAddWord() = flowRouter.startFlow(Screens.WordCreateScreen)
 }
