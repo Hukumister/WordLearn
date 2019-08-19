7ae9e0f54ade06251f5ab7bbcd2e30e99a281860
nmZaltsman <zaltsman.nikita@gmail.com>
2019-08-20 00:26:38 +0700

    Update layout of item learn word Update logic LearnWordsItem

--- app/src/main/java/ru/coderedwolf/wordlearn/ui/learn/word/LearnWordsItem.kt
+++ app/src/main/java/ru/coderedwolf/wordlearn/ui/learn/word/LearnWordsItem.kt
@@ -3,33 +3,59 @@ package ru.coderedwolf.wordlearn.ui.learn.word
 import com.xwray.groupie.kotlinandroidextensions.Item
 import com.xwray.groupie.kotlinandroidextensions.ViewHolder
 import kotlinx.android.synthetic.main.item_learn_word.*
+import org.jetbrains.anko.onClick
 import ru.coderedwolf.wordlearn.R
+import ru.coderedwolf.wordlearn.extension.visible
 import ru.coderedwolf.wordlearn.model.learn.LearnWord
 import ru.coderedwolf.wordlearn.model.word.Word
 
 /**
  * @author CodeRedWolf. Date 22.06.2019.
  */
-class LearnWordsItem(val learnWord: LearnWord) : Item() {
+class LearnWordsItem(
+        val learnWord: LearnWord
+) : Item() {
 
-    override fun bind(viewHolder: ViewHolder, position: Int) = when {
-        learnWord.isNewWord -> bindNewWord(viewHolder, learnWord.word)
-        learnWord.isRotate -> bindRotateWord(viewHolder, learnWord.word)
-        else -> bindSimpleWord(viewHolder, learnWord.word)
+    override fun bind(viewHolder: ViewHolder, position: Int) {
+        viewHolder.wordTranslation.visible(false)
+        viewHolder.visibleButton.visible(true)
+        when {
+            learnWord.isNewWord -> bindNewWord(viewHolder, learnWord.word)
+            learnWord.isRotate -> bindRotateWord(viewHolder, learnWord.word)
+            else -> bindSimpleWord(viewHolder, learnWord.word)
+        }
     }
 
     private fun bindSimpleWord(viewHolder: ViewHolder, word: Word) {
         viewHolder.wordText.text = word.word
         viewHolder.wordTranslation.text = word.translation
+
+        viewHolder.gotItTextView.text = viewHolder.itemView.context.getString(R.string.got_it)
+        viewHolder.missItTextView.text = viewHolder.itemView.context.getString(R.string.missed_it)
+
+        bindVisibleClick(viewHolder)
     }
 
     private fun bindRotateWord(viewHolder: ViewHolder, word: Word) {
         viewHolder.wordText.text = word.translation
         viewHolder.wordTranslation.text = word.word
+
+        bindVisibleClick(viewHolder)
     }
 
     private fun bindNewWord(viewHolder: ViewHolder, word: Word) {
         bindSimpleWord(viewHolder, word)
+
+        viewHolder.gotItTextView.text = viewHolder.itemView.context.getString(R.string.know)
+        viewHolder.missItTextView.text = viewHolder.itemView.context.getString(R.string.not_know)
+        bindVisibleClick(viewHolder)
+    }
+
+    private fun bindVisibleClick(viewHolder: ViewHolder) {
+        viewHolder.visibleButton.onClick {
+            it?.visible(false)
+            viewHolder.wordTranslation.visible(true)
+        }
     }
 
     override fun getLayout(): Int = R.layout.item_learn_word
