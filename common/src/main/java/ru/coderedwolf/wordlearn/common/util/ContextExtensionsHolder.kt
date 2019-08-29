db84744374004cc5f8bdee30e802987ce4ddf094
nmZaltsman <zaltsman.nikita@gmail.com>
2019-08-29 23:45:29 +0700

    Update HasContextExtensions

--- common/src/main/java/ru/coderedwolf/wordlearn/common/util/ContextExtensionsHolder.kt
+++ common/src/main/java/ru/coderedwolf/wordlearn/common/util/ContextExtensionsHolder.kt
@@ -13,18 +13,18 @@ import androidx.core.content.ContextCompat
 import androidx.core.content.res.ResourcesCompat
 import androidx.core.graphics.drawable.DrawableCompat
 
-interface HasContextExtensions {
+interface ContextExtensionsHolder {
 
-    fun getContextForExtensions(): Context
+    val extensionContext: Context
 
-    private fun getResources(): Resources = getContextForExtensions().resources
+    private fun getResources(): Resources = extensionContext.resources
 
     @CheckResult
-    fun @receiver:StringRes Int.textRes(): CharSequence = getContextForExtensions().getText(this)
+    fun @receiver:StringRes Int.textRes(): CharSequence = extensionContext.getText(this)
 
     @CheckResult
     fun @receiver:StringRes Int.stringRes(arg: Any? = null, vararg args: Any?): String =
-            if (arg == null) getContextForExtensions().getString(this) else getContextForExtensions().getString(this, arg, *args)
+            if (arg == null) extensionContext.getString(this) else extensionContext.getString(this, arg, *args)
 
     @CheckResult
     fun @receiver:PluralsRes Int.pluralRes(quantity: Int, vararg formatArgs: Any): String =
@@ -32,10 +32,10 @@ interface HasContextExtensions {
 
     @CheckResult
     fun @receiver:DrawableRes Int.drawableRes(@ColorRes tintColorRes: Int = 0): Drawable? {
-        val drawable = AppCompatResources.getDrawable(getContextForExtensions(), this)
+        val drawable = AppCompatResources.getDrawable(extensionContext, this)
         if (tintColorRes != 0 && drawable != null) {
             val wrappedDrawable = DrawableCompat.wrap(drawable)
-            val color = ContextCompat.getColor(getContextForExtensions(), tintColorRes)
+            val color = ContextCompat.getColor(extensionContext, tintColorRes)
             DrawableCompat.setTint(wrappedDrawable, color)
             return wrappedDrawable
         }
@@ -48,13 +48,13 @@ interface HasContextExtensions {
 
     @CheckResult
     @ColorInt
-    fun @receiver:ColorRes Int.colorRes(): Int = ContextCompat.getColor(getContextForExtensions(), this)
+    fun @receiver:ColorRes Int.colorRes(): Int = ContextCompat.getColor(extensionContext, this)
 
     @CheckResult
-    fun @receiver:ColorRes Int.colorStateListRes(): ColorStateList = AppCompatResources.getColorStateList(getContextForExtensions(), this)!!
+    fun @receiver:ColorRes Int.colorStateListRes(): ColorStateList = AppCompatResources.getColorStateList(extensionContext, this)!!
 
     @CheckResult
-    fun @receiver:FontRes Int.fontRes(): Typeface? = ResourcesCompat.getFont(getContextForExtensions(), this)
+    fun @receiver:FontRes Int.fontRes(): Typeface? = ResourcesCompat.getFont(extensionContext, this)
 
     @CheckResult
     fun @receiver:AnyRes Int.toResourceUri(): Uri = Uri.Builder()
@@ -65,12 +65,12 @@ interface HasContextExtensions {
             .build()
 
     @CheckResult
-    fun @receiver:AttrRes Int.dimensionPixelAttr(context: Context = getContextForExtensions()): Int = Themes.resolveDimensionAttr(context, this)
+    fun @receiver:AttrRes Int.dimensionPixelAttr(context: Context = extensionContext): Int = Themes.resolveDimensionAttr(context, this)
 
     @CheckResult
-    fun @receiver:AttrRes Int.colorAttr(context: Context = getContextForExtensions()): Int = Themes.resolveColorAttr(context, this)
+    fun @receiver:AttrRes Int.colorAttr(context: Context = extensionContext): Int = Themes.resolveColorAttr(context, this)
 
     @CheckResult
-    fun @receiver:AttrRes Int.drawableAttr(context: Context = getContextForExtensions()): Drawable? = Themes.resolveDrawableAttr(context, this)
+    fun @receiver:AttrRes Int.drawableAttr(context: Context = extensionContext): Drawable? = Themes.resolveDrawableAttr(context, this)
 
 }
