411b73c0eb1148c4f2b5bff05763bd0f665feb46
Nikita <zaltsman.nikita@gmail.com>
2019-10-29 05:38:01 +0700

    Update word set user view layer

--- feature-mainflow/src/main/java/ru/coderedwolf/wordlearn/mainflow/ui/word/set/WordSetUserFragment.kt
+++ feature-mainflow/src/main/java/ru/coderedwolf/wordlearn/mainflow/ui/word/set/WordSetUserFragment.kt
@@ -2,39 +2,63 @@ package ru.coderedwolf.wordlearn.mainflow.ui.word.set
 
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
-import io.reactivex.ObservableSource
-import io.reactivex.Observer
+import io.reactivex.Observable
 import io.reactivex.subjects.PublishSubject
 import kotlinx.android.synthetic.main.fragment_word_user_set.*
+import ru.coderedwolf.mvi.core.MviView
 import ru.coderedwolf.wordlearn.common.ui.BaseFragment
-import ru.coderedwolf.wordlearn.common.ui.event.UiEvent
-import ru.coderedwolf.wordlearn.common.ui.item.BaseAdapter
-import ru.coderedwolf.wordlearn.common.util.autoCleared
+import ru.coderedwolf.wordlearn.common.ui.adapter.ItemAsyncAdapter
+import ru.coderedwolf.wordlearn.common.ui.adapter.delegate.ButtonAdapterDelegate
+import ru.coderedwolf.wordlearn.common.util.unsafeLazy
 import ru.coderedwolf.wordlearn.mainflow.R
+import ru.coderedwolf.wordlearn.mainflow.presentation.set.WordSetUserViewModel
+import ru.coderedwolf.wordlearn.mainflow.presentation.set.WordSetUserViewModel.Action
+import ru.coderedwolf.wordlearn.mainflow.presentation.set.WordSetUserViewState
+import ru.coderedwolf.wordlearn.mainflow.presentation.set.WordSetUserViewState.Item
+import javax.inject.Inject
 
 /**
  * @author CodeRedWolf. Date 11.10.2019.
  */
 class WordSetUserFragment : BaseFragment(),
-    ObservableSource<UiEvent> {
+    MviView<Action, WordSetUserViewState> {
 
     companion object {
 
         fun newInstance() = WordSetUserFragment()
     }
 
+    @Inject lateinit var viewModelFactory: ViewModelProvider.Factory
+
+    private val wordSetUserViewModel: WordSetUserViewModel by viewModels {
+        viewModelFactory
+    }
+
     override val layoutRes: Int = R.layout.fragment_word_user_set
 
+    private val source = PublishSubject.create<Action>()
 
-    private val source = PublishSubject.create<UiEvent>()
+    private val itemAdapter: ItemAsyncAdapter<Item> by unsafeLazy {
+        ItemAsyncAdapter.Builder<Item>()
+            .add(ButtonAdapterDelegate())
+            .build()
+    }
 
-    private var itemAdapter by autoCleared<BaseAdapter>()
+    override fun onResume() {
+        super.onResume()
+        wordSetUserViewModel.bindView(this)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        wordSetUserViewModel.unbindView()
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        itemAdapter = BaseAdapter()
 
         recyclerView.apply {
             setHasFixedSize(true)
@@ -44,6 +68,11 @@ class WordSetUserFragment : BaseFragment(),
 
     }
 
-    override fun subscribe(observer: Observer<in UiEvent>) = source.subscribe(observer)
+    override val actions: Observable<Action>
+        get() = source.hide()
+
+    override fun render(state: WordSetUserViewState) {
+
+    }
 
 }
