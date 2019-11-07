04f8ca0308d2ad0e8e70118e3811a1bf6cb275bd
Nikita <zaltsman.nikita@gmail.com>
2019-11-08 01:13:31 +0700

    Update Determinate and Add Product models

--- common/src/main/java/ru/coderedwolf/wordlearn/common/domain/result/Determinate+Extension.kt
+++ common/src/main/java/ru/coderedwolf/wordlearn/common/domain/result/Determinate+Extension.kt
@@ -1,6 +1,5 @@
 package ru.coderedwolf.wordlearn.common.domain.result
 
-import androidx.annotation.IntDef
 import androidx.annotation.IntRange
 import io.reactivex.Completable
 import io.reactivex.Observable
@@ -8,53 +7,24 @@ import io.reactivex.annotations.BackpressureKind
 import io.reactivex.annotations.BackpressureSupport
 import io.reactivex.annotations.CheckReturnValue
 import io.reactivex.annotations.SchedulerSupport
+import ru.coderedwolf.wordlearn.common.util.asObservable
 
 /**
  * @author CodeRedWolf. Date 24.08.2019.
  */
 sealed class Determinate {
 
-    @State
-    abstract val state: Int
-
     @IntRange(from = 0L)
     open val timestamp: Long = System.currentTimeMillis()
 
-    companion object {
+    object Loading : Determinate()
 
-        const val STATE_LOADING = 1
-        const val STATE_COMPLETED = 1 shl 1
-        const val STATE_ERROR = 1 shl 2
-
-        @IntDef(
-                STATE_LOADING,
-                STATE_COMPLETED,
-                STATE_ERROR
-        )
-        annotation class State
-
-    }
-
-    object Loading : Determinate() {
-
-        override val state: Int = STATE_LOADING
-
-    }
-
-    object Completed : Determinate() {
-
-        override val state: Int = STATE_COMPLETED
-
-    }
+    object Completed : Determinate()
 
     data class Error(
-            val throwable: Throwable,
-            override val timestamp: Long = System.currentTimeMillis()
-    ) : Determinate() {
-
-        override val state: Int = STATE_ERROR
-
-    }
+        val throwable: Throwable,
+        override val timestamp: Long = System.currentTimeMillis()
+    ) : Determinate()
 
 }
 
@@ -65,8 +35,6 @@ sealed class Determinate {
 @CheckReturnValue
 @BackpressureSupport(BackpressureKind.UNBOUNDED_IN)
 @SchedulerSupport(SchedulerSupport.NONE)
-fun Completable.asDeterminate(): Observable<Determinate> = andThen(Observable.just<Determinate>(
-        Determinate.Completed
-))
-        .startWith(Determinate.Loading)
-        .onErrorReturn { throwable -> Determinate.Error(throwable) }
+fun Completable.asDeterminate(): Observable<Determinate> = andThen(Determinate.Completed.asObservable<Determinate>())
+    .startWith(Determinate.Loading)
+    .onErrorReturn { throwable -> Determinate.Error(throwable) }
