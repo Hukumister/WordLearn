5e41ef749f1c7a50c9d390a66e4b0e00ba22ec9d
Nikita <zaltsman.nikita@gmail.com>
2019-11-25 01:49:59 +0700

    Add first simple test

---
+++ mvi-core/src/test/kotlin/ru/coderedwolf/mvi/core/impl/TestModels.kt
@@ -0,0 +1,11 @@
+package ru.coderedwolf.mvi.core.impl
+
+/**
+ * @author CodeRedWolf.
+ */
+
+sealed class TestAction
+
+sealed class TestViewEvent
+
+sealed class TestEffect
---
+++ mvi-core/src/test/kotlin/ru/coderedwolf/mvi/core/impl/TestViewState.kt
@@ -0,0 +1,8 @@
+package ru.coderedwolf.mvi.core.impl
+
+/**
+ * @author CodeRedWolf.
+ */
+data class TestViewState(
+    val data: List<String> = emptyList()
+)
