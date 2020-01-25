20c4d91ee47858d80481f3991c1daef0632b5219
NikitaZaltsman <zaltsman.nikita@gmail.com>
2020-01-25 13:45:00 +0700

    Add base implementations of store

--- mvi-core/src/main/java/ru/coderedwolf/mvi/core/JustReducerStore.kt
+++ mvi-core/src/main/java/ru/coderedwolf/mvi/core/JustReducerStore.kt
@@ -1,29 +1,27 @@
 package ru.coderedwolf.mvi.core
 
 import io.reactivex.Flowable
-import io.reactivex.Scheduler
+import ru.coderedwolf.mvi.core.elements.Bootstrapper
 import ru.coderedwolf.mvi.core.elements.EventProducer
 import ru.coderedwolf.mvi.core.elements.Middleware
 import ru.coderedwolf.mvi.core.elements.Reducer
 
-class JustReducerStore<Action : Any, State : Any, Event : Any>(
+open class JustReducerStore<Action : Any, State : Any, Event : Any>(
     initialState: State,
-    mainScheduler: Scheduler,
-    reducerScheduler: Scheduler,
     reducer: Reducer<State, Action>,
+    bootstrapper: Bootstrapper<Action>? = null,
     eventProducer: EventProducer<State, Action, Event>? = null
-) : AbstractStore<Action, State, Event, Action>(
+) : OnlyActionStore<Action, State, Event>(
     initialState = initialState,
-    reducerScheduler = reducerScheduler,
-    mainScheduler = mainScheduler,
     reducer = reducer,
     middleware = BypassMiddleware<State, Action>(),
+    bootstrapper = bootstrapper,
     eventProducer = eventProducer
 ) {
 
     class BypassMiddleware<State : Any, Action : Any> : Middleware<Action, State, Action> {
 
-        override fun invoke(action: Action, state: State): Flowable<Action> =
-            Flowable.just(action)
+        override fun invoke(action: Action, state: State): Flowable<Action> = Flowable.just(action)
     }
+
 }
--- mvi-core/src/main/java/ru/coderedwolf/mvi/core/OnlyActionStore.kt
+++ mvi-core/src/main/java/ru/coderedwolf/mvi/core/OnlyActionStore.kt
@@ -5,13 +5,13 @@ import ru.coderedwolf.mvi.core.elements.EventProducer
 import ru.coderedwolf.mvi.core.elements.Middleware
 import ru.coderedwolf.mvi.core.elements.Reducer
 
-open class BaseStore<Action : Any, State : Any, Event : Any, Effect : Any>(
+open class OnlyActionStore<Action : Any, State : Any, Event : Any>(
     initialState: State,
-    reducer: Reducer<State, Effect>,
-    middleware: Middleware<Action, State, Effect>,
+    reducer: Reducer<State, Action>,
+    middleware: Middleware<Action, State, Action>,
     bootstrapper: Bootstrapper<Action>? = null,
-    eventProducer: EventProducer<State, Effect, Event>? = null
-) : AbstractStore<Action, State, Event, Effect>(
+    eventProducer: EventProducer<State, Action, Event>? = null
+) : BaseStore<Action, State, Event, Action>(
     initialState = initialState,
     reducer = reducer,
     middleware = middleware,
